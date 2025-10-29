45e98482096a09c75b3823962e99ff60317a0fd2
Alexpulgar <alexanderpulgar48@gmail.com>
2025-10-29 17:34:56 -0300

    valida si hay token, si hay, se salta el login

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-mobile/app/navigation/StackNavigator.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-mobile/app/navigation/StackNavigator.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import TabNavigator from "./TabNavigator";
 import ReserveServiceScreen from "../screens/ReserveServiceScreen";
 import colors from "../theme/colors";
-import LoginScreen from "../screens/LoginScreen"; 
+import LoginScreen from "../screens/LoginScreen";
+import { getToken } from "../../services/usuarioService";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [initialRoute, setInitialRoute] = useState('Login');
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await getToken();
+        if (token) {
+          console.log("Token found, navigating to MainTabs");
+          setInitialRoute('MainTabs');
+        } else {
+          console.log("No token found, navigating to Login");
+          setInitialRoute('Login');
+        }
+      } catch (error) {
+        console.error("Error checking token:", error);
+        setInitialRoute('Login');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color={colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login" 
+        initialRouteName={initialRoute}
         screenOptions={{
           headerStyle: { backgroundColor: colors.primary },
           headerTintColor: colors.white,
@@ -25,13 +60,11 @@ export default function StackNavigator() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-
         <Stack.Screen
           name="MainTabs"
           component={TabNavigator}
           options={{ headerShown: false }}
         />
-
         <Stack.Screen
           name="Reserva"
           component={ReserveServiceScreen}
@@ -41,3 +74,12 @@ export default function StackNavigator() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
