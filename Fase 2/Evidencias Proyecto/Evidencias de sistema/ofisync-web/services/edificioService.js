b9d579d19a32682ec04a242de1d660763a346f94
Alexpulgar <alexanderpulgar48@gmail.com>
2025-11-15 22:13:02 -0300

    correccion syntax

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-web/services/edificioService.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-web/services/edificioService.js
@@ -1,52 +1,51 @@
 const API_URL = "http://localhost:4000/api/edificios";
 
 export async function getEdificios() {
-    const res = await fetch(API_URL);
-    return res.json();
+  const res = await fetch(API_URL);
+  return res.json();
 }
 
 export async function agregarEdificioApi(edificio) {
-    const res = await fetch(`${API_URL}/agregar`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(edificio)
-    });
-    return res.json();
+  const res = await fetch(`${API_URL}/agregar`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(edificio),
+  });
+  return res.json();
 }
 
 // (La necesitas para rellenar el formulario)
 export async function getEdificioByIdApi(id) {
-    const res = await fetch(`${API_URL}/${id}`);
-    if (!res.ok) throw new Error('Error al obtener datos del edificio');
-    return res.json();
+  const res = await fetch(`${API_URL}/${id}`);
+  if (!res.ok) throw new Error("Error al obtener datos del edificio");
+  return res.json();
 }
 
 // (La necesitas para guardar)
 export async function actualizarEdificioApi(id, edificioData) {
-    const res = await fetch(`${API_URL}/${id}`, { 
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(edificioData)
-    });
-    if (!res.ok) {
-        const errorData = await res.json();
-        throw new Error(errorData.error || 'Error al actualizar');
-    }
-    return res.json();
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(edificioData),
+  });
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.error || "Error al actualizar");
+  }
+  return res.json();
 }
 
 export async function eliminarEdificioApi(id) {
   const res = await fetch(`${API_URL}/${id}`, {
-    method: "DELETE"
+    method: "DELETE",
   });
-  
+
   const data = await res.json(); // Lee la respuesta (sea error o éxito)
-  
+
   if (!res.ok) {
     // Captura el error (ej: "No se puede eliminar...")
     throw new Error(data.error || "Error al eliminar el edificio");
   }
-  
+
   return data; // Devuelve el mensaje de éxito
 }
-
