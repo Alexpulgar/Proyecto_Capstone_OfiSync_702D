f7ace9c2dcfd12b74da0f55b037188ab28477b06
Alexpulgar <alexanderpulgar48@gmail.com>
2025-11-30 17:15:45 -0300

    ultimos cambios web y posibles ultimos app

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-mobile/app/api/api.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/ofisync-mobile/app/api/api.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { getToken } from "../../services/usuarioService";
+
 const API_URL = "https://desire-holophytic-regretfully.ngrok-free.dev/api";
 
 const API = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
 API.interceptors.request.use(
   async (config) => {
-    // 1. Obtener el token de AsyncStorage antes de cada petición
     const token = await getToken();
 
-    // 2. Si el token existe, añadirlo a la cabecera Authorization
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
